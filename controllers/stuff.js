0d86bf4cf57d08aabb5b739cdd9c9946d2f434ae
profbuchi <profbuchi@gmail.com>
2022-04-16 19:56:47 +0200

    résolution faille sécurité delete avec auth

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -40,12 +40,36 @@ exports.modifyThing = (req, res, next) => {
 };
 
 //une route pour supprimer un objet dont l'id est dans la requête
+
 exports.deleteThing = (req, res, next) => {
-    //maj du thing avec 1er param : son id, 2eme param :spread de l'objet modifié qu'il faudra utiliser
-    Thing.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'objet supprimé !' }))
-        .catch(error => res.status(400).json({ error }));
+    //on extrait de l'objet à effacer le user id (pour vérifier qui vaut effacer)
+    Thing.findOne({ _id: req.params.id })
+        .then(
+            thing => {
+                //on vérifie s'il y a quelque chose à effacer
+                if (!thing) {
+                    res.status(404).json({
+                        error: new Error('Cet objet n\'existe pas !')
+                    });
+                }
+                //si id de l'objet different de celui autentifié
+                if (thing.userId !== req.auth.userId) {
+                    res.status(400).json({
+                        error: new Error('Requête non autorisée !')
+                    });
+                }
+                //maj du thing avec 1er param : son id, 2eme param :spread de l'objet modifié qu'il faudra utiliser
+                Thing.deleteOne({ _id: req.params.id })
+                    .then(() => {
+                        res.status(200).json({ message: 'objet supprimé !' });
+                    })
+                    .catch(error => {
+                        res.status(400).json({ error });
+                    });
+            }
+        )
 };
+
 
 //definition du middleware appelé pour la route vers stuff
 //renvoie en json tout ce qui est dans la base mongoose (lecture bdd) et un status 200
--- middleware/auth.js
+++ middleware/auth.js
@@ -10,6 +10,8 @@ module.exports = (req, res, next) => {
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         //on récupère dans l'objet l'userId
         const userId = decodedToken.userId;
+        //on rajoute ce user id certifié dans la requete sous la forme d'un objet (pour protéger delete)
+        req.auth = { userId: userId };
         //s'il y a un user id et qu'il ne correspond pas à celui décodé dans le token
         if (req.body.userId && req.body.userId !== userId) {
             throw 'Invalid user ID';   //on jette une erreur
@@ -17,7 +19,7 @@ module.exports = (req, res, next) => {
             next(); //tout est bon on enchaine avec la route
         }
         //si le try ne marche pas 
-    } catch  {
+    } catch {
         res.status(401).json({
             error: new Error('Invalid request!')
         });
